5ec6e0df3b0f7f93421e50dba14eed44d5e7f5f9
Pandimuthu <muthupandi95.mp@gmail.com>
2016-06-29 21:16:04 +0530

    Create EvenorOdd.c

---
+++ EvenorOdd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main(){
+int n;
+clrscr();
+printf("Enter the no");
+scanf("%d",&n);
+if(n%2 == 0)
+printf("Even no");
+else
+printf("Odd no");
+getch();
+}
